0d636b88a6c5dc62c114734c930d0fce99a257b5
Xavier Guerrero <144020944+GuerreroDIM46@users.noreply.github.com>
2023-10-12 09:12:47 +0200

    proyectil

--- src/proyectil/EjecucionProjectil.java
+++ src/proyectil/EjecucionProjectil.java
@@ -6,21 +6,28 @@ public class EjecucionProjectil {
 
 	public static void main(String[] args) {
 		
-		Scanner entradaScanner = new Scanner(System.in);
-		
-		System.out.println("introduce altura inicial");
-		double startingHeight = Double.parseDouble(entradaScanner.nextLine());
-
-		System.out.println("introduce velocidad inicial");
-		double startingVelocity = Double.parseDouble(entradaScanner.nextLine());
-		
-		System.out.println("introduce angulo de tiro");
-		double initialAngle = Double.parseDouble(entradaScanner.nextLine());
-		
-		
-		Projectile pruebaProjectile = new Projectile(100, 0, 0);
+//		Scanner entradaScanner = new Scanner(System.in);
+//		
+//		System.out.println("introduce altura inicial");
+//		double startingHeight = Double.parseDouble(entradaScanner.nextLine());
+//
+//		System.out.println("introduce velocidad inicial");
+//		double startingVelocity = Double.parseDouble(entradaScanner.nextLine());
+//		
+//		System.out.println("introduce angulo de tiro");
+//		double initialAngle = Double.parseDouble(entradaScanner.nextLine());
+//		
+		Projectile pruebaProjectile = new Projectile(15, 12, 80);		
+//		Projectile pruebaProjectile = new Projectile(startingHeight, startingVelocity, initialAngle);
 		System.out.println(pruebaProjectile.toString());
 		System.out.println(pruebaProjectile.heightEq());
+		System.out.println(pruebaProjectile.horizEq());
+		System.out.println(pruebaProjectile.height(0.2));
+		System.out.println(pruebaProjectile.horiz(0.2));
+		for (int i = 0; i < pruebaProjectile.landing().length; i++) {
+			System.out.print(pruebaProjectile.landing()[i] + "\t");
+			
+		}
 	}
 
 }
--- src/proyectil/Projectile.java
+++ src/proyectil/Projectile.java
@@ -1,9 +1,12 @@
 package proyectil;
 
 public class Projectile {
-	private double startingHeight;
-	private double startingVelocity ;
-	private double initialAngle;
+	private double startingHeight = 0;
+	private double startingVelocity = 0;
+	private double initialAngle = 0;
+	private double startingVelocityX;
+	private double startingVelocityY;
+	
 	
 	public void setStartingHeight(double startingHeight) {
 		if (0 <= startingHeight && startingHeight < 200) {
@@ -36,20 +39,56 @@ public class Projectile {
 		setStartingHeight(startingHeight);
 		setStartingVelocity(startingVelocity);
 		setInitialAngle(initialAngle);
+		startingVelocityX = startingVelocity * Math.cos(Math.toRadians(initialAngle));
+		startingVelocityY = startingVelocity * Math.sin(Math.toRadians(initialAngle));
+		
 	}
 	
 	public String heightEq() {
 		String result = "holaquetal";
-		result = "";
+		String startingHeightString = "";
+		if (startingHeight > 0) startingHeightString = " + " + String.valueOf(startingHeight);
+		result = "h(t) = -16.0t^2 + " + Math.round(startingVelocityY*1000.0)/1000.0 + "t" + startingHeightString;
 		return result;
-				
 	}
-
+	
+	public String horizEq() {
+		String result = "holaquetal";
+		result = "x(t) = " + Math.round(startingVelocityX*1000.0)/1000.0 +"t";
+		return result;
+	}
+	
+	public double height (double t) {
+		
+		double result = Math.round((- 16 * Math.pow(t, 2) + t * startingVelocityY + startingHeight)*1000.0)/1000.0;
+		return result;
+	}
+	
+	public double horiz(double t) {
+		double result;
+		result = Math.round(t * startingVelocityX * 1000) / 1000.0 ;
+		return result;
+	}
+	
+	public double[] landing() {
+		double x, y, t;
+		y = 0;
+		t = ((-startingVelocityY) - Math.sqrt((Math.pow(startingVelocityY, 2) - (4 * (-16) * startingHeight)))) / (2 * (-16));
+		x = Math.round(startingVelocityX * t * 1000.0) / 1000.0;
+		t = Math.round(t *1000.0) / 1000.0;
+		double[] result = new double[] {x, y, t};
+		return result;
+		
+	}
+	
 	@Override
 	public String toString() {
-		return "Projectile [startingHeight=" + startingHeight + ", startingVelocity=" + startingVelocity
-				+ ", initialAngle=" + initialAngle + "]";
+		return "Projectile [startingHeight=" + startingHeight + ", startingVelocityX="
+				+ startingVelocityX + ", startingVelocityY=" + startingVelocityY + "]";
 	}
+
+
+	
 	
 	
 }
